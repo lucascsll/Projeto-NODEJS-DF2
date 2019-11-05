acfd0ce8a752318cc9a5fe98d76be8c415d5d298
Lucas <lucas.cordeiro@mobilesaude.com.br>
2019-11-05 20:48:41 -0300

    first commit

--- SRC/config/database.js
+++ SRC/config/database.js
@@ -1,9 +1,11 @@
+require('dotenv/config');
+
 module.exports = {
-  dialect: 'postgres',
-  host: 'localhost',
-  username: 'postgres',
-  password: 'docker',
-  database: 'gobarber',
+  dialect: 'mysql',
+  host:  process.env.HOST,
+  username: process.env.USER_NAME,
+  password:  process.env.PASSWORD,
+  database:  process.env.DATABASE,
   define: {
     timestamps: true,
     underscored: true,
